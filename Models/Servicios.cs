033110db849c17398820118b03702059a82cae4d
Ren4ta <48313480@est.ort.edu.ar>
2024-10-21 10:11:07 -0300

    models

---
+++ Models/Servicios.cs
@@ -0,0 +1,9 @@
+public class Servicios 
+{
+    public int ID_Servicio {get; set;}
+    public int DNI_Usuario1 {get; set;}
+    public string Nombre_Usuario1 {get; set;} 
+    public string Descripcion {get; set;} 
+    public int DNI_Usuario2 {get; set;}
+    public string Nombre_Usuario2 {get; set;}
+}
---
+++ Models/Trabajadores.cs
@@ -0,0 +1,7 @@
+public class Trabajadores 
+{
+    public int ID_Trabajador {get; set;} 
+    public string Matricula {get; set;} 
+    public string Especialidad {get; set;} 
+    public int DNI {get; set;} 
+}
