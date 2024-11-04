cc987b72e9ef3e077479e63d9dcfd86379bdaa8c
Ren4ta <48313480@est.ort.edu.ar>
2024-11-04 08:34:17 -0300

    Arreglos models + agregar clases

---
+++ Models/ServiciosXTrabajador.cs
@@ -0,0 +1,7 @@
+class ServiciosXTrabajador 
+{
+    public int ID_Serv_X_Tra {get; set;} 
+    public int ID_Cliente {get; set;} 
+    public int ID_Trabajador {get; set;} 
+    public int ID_Servicio {get; set;}
+}
--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -3,11 +3,17 @@ public class Usuarios
     public int DNI {get; set;} 
     public string Nombre {get; set;} 
     public string Apellido {get; set;} 
-    public string Direccion {get; set;} 
-    public int Edad  {get; set;} 
-    public int Calificacion {get; set;} 
+
+    public int Edad  {get; set;}  
+
+    public int ID_Valoracion {get; set;}
+   
     public bool Trabajador {get; set;} 
-    public string Email {get; set;}
-    public string Password {get; set;}
-    public int Numberuser {get; set;}
+
+     public string Email {get; set;}
+    public string Password {get; set ;} 
+
+    public int ID_Usuario {get; set;} 
+
+    public string Foto {get; set;}
 }
