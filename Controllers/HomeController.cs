3ec7e5106410e4bec1b1e453351758e8db1deb57
EliasSuez <48240167@est.ort.edu.ar>
2024-11-06 08:22:33 -0300

    Uso de cookies en log in

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,6 +34,18 @@ public class HomeController : Controller
         Usuarios nuevoUsuario = Utilities.CrearUsuario(dni, nombre, apellido, email, password, direccion, fecha);
         return RedirectToAction("Index");
     }
+
+    [Route("/Home/SingUp")]
+    public IActionResult SignUp()
+    {
+        Response.Cookies.Delete("UserId");
+        Response.Cookies.Append("UserId", "123456");
+        string user = Request.Cookies["UserId"];
+
+        Console.WriteLine(user ?? " No hay usuariodefinido");
+        return View();
+    }
+
     public IActionResult Register()
     {
         return View();
