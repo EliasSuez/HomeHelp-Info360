b4be69cc354daee0bae4dc5851356b30aa9b1b11
Ren4ta <48313480@est.ort.edu.ar>
2024-11-25 09:36:20 -0300

    arreglos

--- Models/ServiciosXTrabajador.cs
+++ Models/ServiciosXTrabajador.cs
@@ -3,5 +3,8 @@ public class ServiciosXTrabajador
     public int ID_Serv_X_Tra {get; set;} 
     public int ID_Cliente {get; set;} 
     public int ID_Trabajador {get; set;} 
-    public int ID_Servicio {get; set;}
+    
+    public string nombre_servicios{get; set;} 
+
+    public DateTime fechahora {get; set;}
 }
